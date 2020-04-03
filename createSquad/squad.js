56cd4e4ae31762c387e8800a9086a55aa681eadb
exhex <c.kritik@gmail.com>
2020-04-03 15:17:23 +0200

    imported jQuery, added stick navBar

---
+++ createSquad/squad.js
@@ -0,0 +1,10 @@
+let $navButtons = $("#navGrid").children("a");
+
+
+
+onload = function()
+{
+    // lets do things to the navButtons!
+    $navButtons.addClass("navButton");  // give them style
+                                        // maybe give them a href to parts of the page?
+}
