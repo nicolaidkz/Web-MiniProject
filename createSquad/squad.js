4aacd6d73bebeb3bf97a35f80d29fbee03e7adff
Carsten <c.kritik@gmail.com>
2020-04-03 21:17:01 +0200

    Added sliding squad card

--- createSquad/squad.js
+++ createSquad/squad.js
@@ -1,10 +1,24 @@
-let $navButtons = $("#navGrid").children("a");
-
-
+let $navButtons = $("#navGrid").children("a");                              // get all a-tag children of navGrid
+var navButtonRef = ["squad.html", undefined, ""];                           // button reference strings
+let $squadCard = $("#squadCard");
 
 onload = function()
 {
     // lets do things to the navButtons!
-    $navButtons.addClass("navButton");  // give them style
-                                        // maybe give them a href to parts of the page?
+    $navButtons.addClass("navButton");                                      // give them style
+    $navButtons.each(function(index)
+    {
+        if(navButtonRef[index])                                             // is it TRUTHY ? (not true)
+        {
+            $(this).attr("href", navButtonRef[index]);                      // maybe give them a href?
+        }
+        else console.log(`missing URL for navButton  ${(index+1)} in navButtonRef!`);     
+    });      
 }
+
+function ToggleSquadCard()
+{
+    console.log("clickety");
+    $squadCard.toggleClass("expand collapse");
+}
+
